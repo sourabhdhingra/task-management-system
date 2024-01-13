0271c8908addcc9182c517fea80b044cfcae299c
Sourabh Dhingra <sourabhdhingra91@gmail.com>
2024-01-13 11:55:30 +0530

    starting commnand line interface

---
+++ src/main/scala/CommandLineInterface.scala
@@ -0,0 +1,8 @@
+class CommandLineInterface {
+  val entryMessage =
+    s"""
+       |Welcome to the Task Management System!
+       |Please select an operation below:
+       |1. Add Task
+       |2. """.stripMargin
+}
