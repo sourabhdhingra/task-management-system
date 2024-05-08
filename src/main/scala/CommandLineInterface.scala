bf77f045dee79366252e066024b1f07e9718a16a
sourabhdhingra <sourabhdhingra91@gmail.com>
2024-05-08 22:16:43 +0530

    added func for user input and unit TC

--- src/main/scala/CommandLineInterface.scala
+++ src/main/scala/CommandLineInterface.scala
@@ -1,8 +1,33 @@
 class CommandLineInterface {
-  val entryMessage =
+  private val entryMessage =
     s"""
        |Welcome to the Task Management System!
        |Please select an operation below:
-       |1. Add Task
-       |2. """.stripMargin
+       |1. List all tasks
+       |2. Add a new task
+       |3. View an existing task
+       |4. Mark a task complete
+       |5. Delete an existing task
+       |6. View tasks by status
+       |""".stripMargin
+
+  def getEntryMessage(): String = {
+    entryMessage
+    }
+
+  def printEntryMessage(): Unit = {
+    println(entryMessage)
+  }
+
+  def getUserInput(): Int = {
+    val input = scala.io.StdIn.readInt()
+    input
+  }
+}
+
+@main
+def main() = {
+  val cli = CommandLineInterface()
+  cli.printEntryMessage()
+  println(cli.getUserInput())
 }
---
+++ src/test/scala/CommandLineInterfaceTests.scala
@@ -0,0 +1,31 @@
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers.shouldEqual
+
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
+
+class CommandLineInterfaceTests extends AnyFunSuite{
+  test("Able to print CLI entry message") {
+    val entryMessage = CommandLineInterface().getEntryMessage()
+    entryMessage shouldEqual
+      s"""
+         |Welcome to the Task Management System!
+         |Please select an operation below:
+         |1. List all tasks
+         |2. Add a new task
+         |3. View an existing task
+         |4. Mark a task complete
+         |5. Delete an existing task
+         |6. View tasks by status
+         |""".stripMargin
+  }
+
+  test("Able to take user input in Int") {
+    val input: Int = 5
+    val mockInput = new ByteArrayInputStream(input.toString.getBytes)
+    var result:Int = 0
+    Console.withIn(mockInput) {
+      result = CommandLineInterface().getUserInput()
+    }
+    result shouldEqual input
+  }
+}
